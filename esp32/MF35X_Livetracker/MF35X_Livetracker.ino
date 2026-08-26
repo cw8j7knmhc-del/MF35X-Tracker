30e1ac0a426e762540e7c45403836d70bd3937b5
cw8j7knmhc-del <cw8j7knmhc@privaterelay.appleid.com>
2026-08-26 18:23:44 +0200

    Trigger signed V5.9.14 firmware build

--- esp32/MF35X_Livetracker/MF35X_Livetracker.ino
+++ esp32/MF35X_Livetracker/MF35X_Livetracker.ino
@@ -5,5 +5,7 @@
   Der eigentliche Tracker-Code liegt in MF35X_Livetracker_core.hpp.
   Dadurch kann der Arduino-.ino-Praeprozessor keine automatisch erzeugten
   Funktionsprototypen mit falschem C/C++-Linkage erzeugen.
+
+  Build-Trigger fuer den signierten V5.9.14-OTA-Build.
 */
 #include "MF35X_Livetracker_core.hpp"
