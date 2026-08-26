9eedc9d95b12836348acaaf33db13417ea41d959
cw8j7knmhc-del <cw8j7knmhc@privaterelay.appleid.com>
2026-08-26 06:30:23 +0200

    Add MF35X web push public-key config

---
+++ push-config.js
@@ -0,0 +1,5 @@
+/* MF35X Web Push V9.6.0
+ * Hier wird NUR der oeffentliche Web-Push/VAPID-Schluessel eingetragen.
+ * Niemals einen privaten VAPID-Schluessel in GitHub speichern.
+ */
+export const MF35X_VAPID_PUBLIC_KEY = "HIER_FIREBASE_WEB_PUSH_PUBLIC_KEY_EINTRAGEN";
