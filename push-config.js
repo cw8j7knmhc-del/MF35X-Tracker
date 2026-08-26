cc11c44ed7662b1c011d1fc24d2bb711ae5ec629
cw8j7knmhc-del <cw8j7knmhc@privaterelay.appleid.com>
2026-08-26 09:39:51 +0200

    Add Web Push public key configuration placeholder

---
+++ push-config.js
@@ -0,0 +1,5 @@
+// MF35X Background-Push
+// Hier wird nach Aktivierung von Firebase Cloud Messaging ausschliesslich
+// der OEFFENTLICHE Web-Push/VAPID-Schluessel eingetragen.
+// Der private Schluessel gehoert niemals in dieses Repository.
+export const FIREBASE_WEB_PUSH_PUBLIC_KEY = "";
