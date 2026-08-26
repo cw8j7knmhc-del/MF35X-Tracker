b5cf8fe2940db20303f22993fd125face1a6c1ce
cw8j7knmhc-del <cw8j7knmhc@privaterelay.appleid.com>
2026-08-26 18:18:37 +0200

    Prepare V5.9.14 RPM calibration build

--- esp32/MF35X_Livetracker/MF35X_Livetracker.ino
+++ esp32/MF35X_Livetracker/MF35X_Livetracker.ino
@@ -1,5 +1,5 @@
 /*
-  MF35X Livetracker V5.9.13 OTA SIGNED - OFFLINE-RENNSPEICHER + NETZUNABHAENGIGE GPIO11-STEUERUNG
+  MF35X Livetracker V5.9.14 OTA SIGNED - RPM-KALIBRIERUNG 0.06512
 
   Diese .ino-Datei bleibt absichtlich minimal.
   Der eigentliche Tracker-Code liegt in MF35X_Livetracker_core.hpp.
