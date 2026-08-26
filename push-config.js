2afa3ac98437693865b15e9d7e74c5e767684535
cw8j7knmhc-del <cw8j7knmhc@privaterelay.appleid.com>
2026-08-26 06:42:44 +0200

    Configure Firebase Web Push public key

--- push-config.js
+++ push-config.js
@@ -2,4 +2,4 @@
  * Hier wird NUR der oeffentliche Web-Push/VAPID-Schluessel eingetragen.
  * Niemals einen privaten VAPID-Schluessel in GitHub speichern.
  */
-export const MF35X_VAPID_PUBLIC_KEY = "HIER_FIREBASE_WEB_PUSH_PUBLIC_KEY_EINTRAGEN";
+export const MF35X_VAPID_PUBLIC_KEY = "BEXm_lBkPMah7sHa8OUEnV2yXa0ZLd76691LfvOrIKMsppvmIOAs4HUOTNV56RxJ3C7XN1oihdAYfuh0ko0Qzc8";
