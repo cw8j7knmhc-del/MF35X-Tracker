d61a2791dc5db67dbca0c9d524ec86f5074d732d
cw8j7knmhc-del <cw8j7knmhc@privaterelay.appleid.com>
2026-08-27 09:43:37 +0200

    Stage A: add public web push config

---
+++ push-config.js
@@ -0,0 +1,5 @@
+/* MF35X Web Push – Stufe A
+ * Nur der öffentliche Firebase-Web-Push/VAPID-Schlüssel.
+ * Kein privater Schlüssel gehört in dieses Repository.
+ */
+export const MF35X_VAPID_PUBLIC_KEY = "BEXm_lBkPMah7sHa8OUEnV2yXa0ZLd76691LfvOrIKMsppvmIOAs4HUOTNV56RxJ3C7XN1oihdAYfuh0ko0Qzc8";
